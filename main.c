8881794e09293c8dfec729b1a6dcf0627ca83a8d
akashbakshi <akashbakshi@bubble-tech.com>
2017-12-13 15:40:18 -0500

    - Added examples in main on how we'll potentially use the functions

--- main.c
+++ main.c
@@ -5,7 +5,12 @@
 
 int main( int argc, char *argv[] ){
 
-    /*testing file io, this will onlybe here temporarily*/
+    /*testing file io, this will onlybe here temporarily; invalid dir*/
     readFile("gg.txt");
+    /*testing file io, valid dir*/
+    readFile("reg-20171203.in");
 
+
+    /*testing file io, outputing to file*/
+    outputFile("CST8234.out");
 }
--- parse.c
+++ parse.c
@@ -4,11 +4,11 @@
 int readFile(char *dir){
     FILE *file = fopen(dir,"r");
     if(file == NULL){
-        printf("DEBUG: File Not Found");
+        printf("DEBUG: File Not Found\n");
         return 0;
     }
     else
-        printf("DEBUG: File Found");
+        printf("DEBUG: File Found\n");
     fclose(file);
 }
 
@@ -16,11 +16,11 @@ int createOutputFile(char *dir){
     FILE *file = fopen(dir,"w");
 
     if(file == NULL){
-        printf("DEBUG: File Not Found Creating one");
+        printf("DEBUG: File Not Found Creating one\n");
         return 0;
     }
     else
-        printf("DEBUG: File Found Overwriting");
+        printf("DEBUG: File Found Overwriting\n");
         
     fclose(file);
 }
